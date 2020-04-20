7122967fc2b4be9c4a0063275f0139cc4834da49
Luna <ljx_6717@126.com>
2020-04-20 13:05:41 +0800

    准备上传了

--- src/x.js
+++ src/x.js
@@ -1,3 +1,5 @@
-import "./x.css";
+import "./x.scss";
+import "./y.scss";
+import "./z.styl";
 
 export default "xxx";
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,4 +14,33 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/,
+        loader: "less-loader", // compiles Less to CSS
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
